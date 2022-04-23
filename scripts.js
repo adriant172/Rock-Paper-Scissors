d2db479b92a3a5b3a9e583041c935bb1a68af921
Matthew <adriant172@gmail.com>
2022-04-23 22:48:25 +0900

    Create function that compares user and computer selections and returns result

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
 function computerPlay() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
     let currentPick = options[Math.floor(Math.random() * options.length)];
     return currentPick;
 }
 
 console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let win;
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            win = false;
+        } else if (computerSelection === "scissors") {
+            win = true;
+        } else {
+            win = "Tie";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            win = true;
+        } else if (computerSelection === "scissors") {
+            win = false;
+        } else {
+            win = "Tie";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            win = false;
+        } else if (computerSelection === "paper") {
+            win = true;
+        } else {
+            win = "Tie";
+        }
+    }
+
+    if (win === true) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (win === false) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return " It's a Tie!";
+    }
+}
